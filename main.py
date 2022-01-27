d47d4b65874253c1ae0dadefbf11fe2c04786f40
denalzm <pingu.denaly@gmail.com>
2022-01-27 13:28:51 -0500

    Finished english to russian
    I am so cool

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Low Quality Phonetic Transliteration Service
 # Version 1.0
 # @denalz#9606
-# 2022/01/25
+# 2022/01/25 - 2022/01/27
 
 import math, random, io, time
 from english import latinContent
@@ -14,12 +14,14 @@ def main():
     print("Version number: " + str(VNUM))
     print("Type \"quit\" to exit at any time.")
     choice = "poopy"
-    while(choice.lower() != "quit"):
+    while choice.lower() != "quit":
         choice = input("Enter your input string: ")
-        if(choice == "quit"):
+        if choice == "quit":
             print("In Russian that would be \"квуит\"\n\n")
             exit(0)
-        elif(latinContent(choice) > 0.5):
+        elif choice == "":
+            print("Please enter something!")
+        elif latinContent(choice) > 0.5:
             print("Latin alphabet detected. Assuming English pronunciations...")
             print("In Russian that would be: ")
             print(cyrillicizeEnglish(choice))
--- russian.py
+++ russian.py
@@ -51,16 +51,159 @@ EN_TO_RU = {"A":"А",
             "w":"в",
             "x":"кс",
             "y":"й",
-            "z":"з",}
+            "z":"з",
+            "'":"ь",
+            "Ya":"Я",
+            "Ye":"Е",
+            "Yi":"И",
+            "Yo":"Ё",
+            "Yu":"Ю",
+            "ya":"я",
+            "ye":"е",
+            "yi":"и",
+            "yo":"ё",
+            "yu":"ю"}
 
 def cyrillicizeEnglish(instring):
-    result = ""
-    for chr in instring:
-        if chr in EN_TO_RU:
-            result += EN_TO_RU[chr]
+    result = []
+    c = 0
+    while c < len(instring):
+        chr = instring[c]
+        if not chr in EN_TO_RU:
+            result.append(chr)
+        elif chr == "'":
+            prev = instring[c - 1]
+            if instring[c - 2:c].lower() == "ts":
+                prev = "ц"
+            result.append(hardsoft(prev, chr))
+        elif chr.lower() == "y":
+            vowels = {"a", "e", "i", "o", "u"}
+            if c == len(instring) - 1 or instring[c + 1] not in vowels:
+                if len(instring) > 1 and instring[c - 1] in vowels:
+                    result.append(EN_TO_RU[chr])
+                else:
+                    if chr.isupper():
+                        result.append("И")
+                    else:
+                        result.append("и")
+                c -= 1
+            elif instring[c + 1].lower() == "a":
+                result.append(EN_TO_RU[chr + "a"])
+            elif instring[c + 1].lower() == "e":
+                result.append(EN_TO_RU[chr + "e"])
+            elif instring[c + 1].lower() == "i":
+                result.append(EN_TO_RU[chr + "i"])
+            elif instring[c + 1].lower() == "o":
+                result.append(EN_TO_RU[chr + "o"])
+            elif instring[c + 1].lower() == "u":
+                result.append(EN_TO_RU[chr + "u"])
+            c += 1
+        elif chr.lower() == "h":
+            if c == 0:
+                result.append(EN_TO_RU[chr])
+            elif instring[c - 1].lower() == "t":
+                continue
+            elif instring[c - 1] == "C":
+                result.pop()
+                result.append("Ч")
+            elif instring[c - 1] == "c":
+                result.pop()
+                result.append("ч")
+            elif instring[c - 1] == "I":
+                result.pop()
+                result.append("Ы")
+            elif instring[c - 1] == "i":
+                result.pop()
+                result.append("ы")
+            elif instring[c - 1] == "K":
+                result.pop()
+                result.append("Х")
+            elif instring[c - 1] == "k":
+                result.pop()
+                result.append("х")
+            elif instring[c - 1] == "P":
+                result.pop()
+                result.append("Ф")
+            elif instring[c - 1] == "p":
+                result.pop()
+                result.append("ф")
+            elif instring[c - 1] == "S":
+                result.pop()
+                result.append("Ш")
+            elif instring[c - 1] == "s":
+                result.pop()
+                result.append("ш")
+            elif instring[c - 1] == "Z":
+                result.pop()
+                result.append("Ж")
+            elif instring[c - 1] == "z":
+                result.pop()
+                result.append("ж")
+            else:
+                result.append(EN_TO_RU[chr])
+        elif chr.lower() == "c":
+            softs = {"e", "i"}
+            if c < len(instring) - 1 and instring[c + 1].lower() in softs:
+                if chr.isupper():
+                    result.append("С")
+                else:
+                    result.append("с")
+            else:
+                result.append(EN_TO_RU[chr])
+        elif chr.lower() == "g":
+            softs = {"e", "i"}
+            if c < len(instring) - 1 and instring[c + 1].lower() in softs:
+                if chr.isupper():
+                    result.append("Ж")
+                else:
+                    result.append("ж")
+            elif c > 0 and instring[c - 1].lower() == "d":
+                if chr.isupper():
+                    result.append("Ж")
+                else:
+                    result.append("ж")
+            else:
+                result.append(EN_TO_RU[chr])
+        elif len(instring) - c > 3 and instring[c:c + 4].lower() == "shch":
+            if chr.isupper():
+                result.append("Щ")
+            else:
+                result.append("щ")
+            c += 3
+        elif len(instring) - c > 1 and instring[c:c + 2].lower() == "ee":
+            if chr.isupper():
+                result.append("И")
+            else:
+                result.append("и")
+            c += 1
+        elif len(instring) - c > 1 and instring[c:c + 2].lower() == "oo":
+            if chr.isupper():
+                result.append("У")
+            else:
+                result.append("у")
+            c += 1
+        elif len(instring) - c > 1 and instring[c:c + 2].lower() == "ts":
+            if chr.isupper():
+                result.append("Ц")
+            else:
+                result.append("ц")
+            c += 1
         else:
-            result += chr
-    return result
+            result.append(EN_TO_RU[chr])
+        c += 1
+    return "".join(result)
+
+def hardsoft(prev, thus):
+    defineds = {"j", "h", "y", "ц"}
+    hards = {}
+    softs = {}
+    if prev in defineds:
+        return ""
+    if thus in hards:
+        return "ь"
+    if thus in softs:
+        return "ъ"
+    return "ь"
 
 # Ё ё Yo yo
 # Ю ю Yu yu
@@ -73,6 +216,6 @@ def cyrillicizeEnglish(instring):
 # Ш ш Sh sh
 # Щ щ Shch shch
 # Ы ы Ih ih
-# Ъ ъ hard sign not implemented
-# Ь ь soft sign not implemented
-# th, ce/ci, ge/gi
+# Ъ ъ hard sign not implemented or '?
+# Ь ь soft sign not implemented or '?
+# th, ce/ci, ge/gi, ee
